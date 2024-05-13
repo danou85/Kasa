87b1263495d64d3c80dc31bda3c56260b6efb1f0
danou85 <111521806+danou85@users.noreply.github.com>
2024-05-13 10:11:06 +0200

    ajout de commentaire

--- src/composant/collapse.jsx
+++ src/composant/collapse.jsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
+// Composant de collapse réutilisable
 const Collapse = ({ title, content }) => {
+  // Utilisation du hook useState pour gérer l'état du collapsible
   const [isCollapsible1Open, setCollapsible1Open] = useState(false);
+
+  // Fonction pour rendre l'icône chevron basée sur l'état actuel
   const renderChevron = (isOpen) => {
     return (
       <FontAwesomeIcon
@@ -12,18 +16,23 @@ const Collapse = ({ title, content }) => {
       />
     );
   };
+
+  // Rendu du composant Collapse
   return (
     <div className="collapse-content">
+      // Bouton pour activer/désactiver le collapsible 
       <button
         className="collapse"
         onClick={() => setCollapsible1Open(!isCollapsible1Open)}
       >
         {title}
+        //Appel à la fonction pour rendre l'icône chevron
         {renderChevron(isCollapsible1Open)}
       </button>
+      // Affichage du contenu si le collapsible est ouvert 
       {isCollapsible1Open && (
         <div className="collapse-description">
-          {/* Contenu de la collapsible 1 */}
+          // Contenu de la collapsible //
           {content}
         </div>
       )}
@@ -31,4 +40,5 @@ const Collapse = ({ title, content }) => {
   );
 };
 
+// Export du composant Collapse pour une utilisation dans d'autres composants
 export default Collapse;
