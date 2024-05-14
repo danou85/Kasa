3e422cff6b07fa3a021c5f7cefa5b156b36ea846
danou85 <111521806+danou85@users.noreply.github.com>
2024-05-14 21:46:03 +0200

    modif  h1

--- src/composant/Header.jsx
+++ src/composant/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     // Rendu du composant Header
     return (
         <header> {/* Élément header pour l'en-tête */}
-            <img src={logo} alt="logo" className="logo"/> {/* Affichage du logo avec la classe 'logo' */}
+           <h1><img src={logo} alt="logo" className="logo"/> </h1> {/* Affichage du logo avec la classe 'logo' */}
             <nav> {/* Élément nav pour la navigation */}
                 {/* Utilise le composant NavLink pour créer un lien vers la page d'accueil avec la classe CSS 'nav-link' */}
                 <NavLink to='/' className='nav-link'>
